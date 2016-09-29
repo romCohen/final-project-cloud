c244beb42dc495f65ce9efe64f89077aa79557b0
romcohen <rom.cohen90@gmail.com>
2016-09-29 13:59:48 +0300

    Added DB

--- final/app.js
+++ final/app.js
@@ -20,10 +20,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+
+// login - to check data
+app.get('/', function (req, res) {
+  res.render('enter.jade');
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
