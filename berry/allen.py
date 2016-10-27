5c8adb51400c204ee88b42a1ed63e921fd3826e8
omerzk <mine!>
2016-10-27 13:44:48 +0300

    minor

--- berry/allen.py
+++ berry/allen.py
@@ -1,19 +1,17 @@
-import os
 import requests
 import re
 import subprocess
-import time
 
 MAC_FILE = "/var/tmp/req.txt"
 SERVER_ENDPOINT = "http://192.168.1.104/"
-SRC_MAC_PATTERN = re.compile("")
+SRC_MAC_PATTERN = re.compile("([\dA-F]{2}(?:[-:][\dA-F]{2}){5})")
 OPERATION_TIME = 40
-CAPTURE_CMD = "sudo timeout " + str(OPERATION_TIME) + " tcpdump -ni mon0 type mgt subtype probe-req"
+CAPTURE_CMD = "sudo timeout " + str(OPERATION_TIME) + " tcpdump  -i mon0 -ni mon0 type mgt subtype probe-req"
 
 with open(MAC_FILE, 'w') as macfile:
     p = subprocess.Popen(CAPTURE_CMD, stdout=macfile, shell=True)
     p.communicate()
-    
+
 macs = []
 with open(MAC_FILE, 'r') as captured:
     print("1111", captured.read())
