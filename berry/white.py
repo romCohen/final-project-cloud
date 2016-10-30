b58fefb28ff18b44a26d595c31c6361310018c7c
omerzk <mine!>
2016-10-30 06:04:44 +0200

    white

--- berry/white.py
+++ berry/white.py
@@ -1,10 +1,16 @@
 import pickle
 import requests
+import os
 
 SERVER_ENDPOINT = "http://192.168.1.104/"
+cachePath = '/var/tmp/pick'
 payload = None
-
-with open('/var/tmp/pick', 'rb') as inFile:
-    payload = pickle.load(inFile)
-print(payload)
-# requests.post(SERVER_ENDPOINT, data=payload)
+if(os.path.isfile(cachePath)):
+    with open(cachePath, 'rb') as inFile:
+        payload = pickle.load(inFile)
+    os.remove(cachePath)
+    print(payload)
+    # requests.post(SERVER_ENDPOINT, data=payload)
+else:
+    print("no Cache")
+    
